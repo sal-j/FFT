37351bb6e06d32963cadea4cc5ba0fe125ec952e
salj <javaid.salman@pnec.nust.edu.pk>
2015-09-21 22:02:20 +0500

    Updated FFT definition and README.

--- fft.py
+++ fft.py
@@ -7,6 +7,7 @@ def printSingleButterfly(start = 0, end = 2):
         print "x" + str(i) + " - " + "x" + str(end + k)
         k = k + 1
 
+# print actual butterflies
 def printButterflies(point = 0, count = [], stage = 1):
     N = point
     k = 0
@@ -21,6 +22,9 @@ def printButterflies(point = 0, count = [], stage = 1):
                printSingleButterfly(start = count[i], end = count[i] + half)
 
 
+# Find location of starting points for
+# each FFT. For example in case of 4 pt.
+# x0 with x2 and x1 with x3.
 def findFactors(point = 8, numBF = 0, stage = 1):
     test = point
     count = []
@@ -29,31 +33,28 @@ def findFactors(point = 8, numBF = 0, stage = 1):
             count.append(0)
         else:
             count.append(count[i - 1] + test)
-            
-    #print count
 
     printButterflies(point = point, count = count, stage = stage)
 
+# calculate number of butterflies
 def numButterFlies(point = 8, stage = 0):
 
     numBF = pow(2, stage)
     print "At stage " + str(stage) + " number of butterflies are " + str(numBF)
     findFactors(point = point, numBF = numBF, stage = stage + 1)
 
+# Recursive function to calculate points
 def fft(point = 8, stage = 0, stages = 3):
+
     if stages > 0:
         numButterFlies(point = point, stage = stage)
         fft(point = point / 2, stage = stage + 1, stages = stages - 1)
 
-    #numButterFlies(point = point, stage = 1)
-    #numButterFlies(point = point, stage = 2)
-    #numButterFlies(point = point, stage = 3)
-
+# Main wrapper function
 def demo():
-    point = 8
+    point = 16
     stages = int(math.log(8)/math.log(2))
     fft(point = point, stages = stages)
-    #findFactors()
 
 if __name__ == '__main__':
     demo()
